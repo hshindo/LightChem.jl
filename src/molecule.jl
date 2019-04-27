c564d11fe6735351a8d8afe2283744481c437b31
Hiroyuki Shindo <hshindo@users.noreply.github.com>
2019-04-27 14:51:08 +0900

    Fix wrtesdf bug

--- src/molecule.jl
+++ src/molecule.jl
@@ -125,7 +125,7 @@ function writesdf(filename::String, mols::Vector{Molecule})
             println(io, "")
             @printf(io, "%03s%03s%03s", natoms(m), nbonds(m), 0)
             for a in m.atoms
-                @printf(io, "%10s%10s%10s %-03s", a.x, a.y, a.z, a,symbol)
+                @printf(io, "%10s%10s%10s %-03s", a.x, a.y, a.z, a.symbol)
             end
             for b in m.bonds
                 @printf(io, "%03s%03s%03s%03s%03s%03s%03s%03s", b.i, b.j, b.type b.stereo, 0, 0, 0, 0)
